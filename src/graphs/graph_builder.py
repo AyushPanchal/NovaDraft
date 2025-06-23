b97cc8cc177f3339afdee023a00439f7ada4ef7b
Ayush <ayush18november@gmail.com>
2025-06-23 15:24:27 +0530

    LANGGRAPH STUDIO SETUP

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -33,3 +33,12 @@ class GraphBuilder:
             self.build_topic_graph()
 
         return self.graph.compile()
+
+
+# Below code is for the langsmith langgraph studio
+
+llm = GroqLLM().get_llm()
+
+graph_builder = GraphBuilder(llm)
+
+graph = graph_builder.build_topic_graph().compile()
