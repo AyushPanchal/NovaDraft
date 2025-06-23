b28d04745d99518970776f01e31cc8ffffd7bb9e
Ayush <ayush18november@gmail.com>
2025-06-23 18:41:24 +0530

    built the language graph, nodes are yet to be implemented

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -28,9 +28,41 @@ class GraphBuilder:
 
         return self.graph
 
+    def build_language_graph(self):
+        """
+        Build a graph for blog generation for input topic and language
+        :return:
+        """
+        self.blog_node = BlogNode(self.llm)
+
+        # Nodes
+        self.graph.add_node("title_creation", self.blog_node.title_creation)
+        self.graph.add_node("content_generation", self.blog_node.content_generation)
+        self.graph.add_node("hindi_translation", )
+        self.graph.add_node("french_translation", )
+        self.graph.add_node("route", )
+
+        # Add edges and conditional edges
+        self.graph.add_edge(START, "title_creation")
+        self.graph.add_edge("title_creation", "content_generation")
+        self.graph.add_edge("content_generation", "route")
+
+        # Conditional edge
+        self.graph.add_conditional_edges("route",
+                                         self.blog_node.route_decision,
+                                         {"hindi": "hindi_translation", "french": "french_translation"})
+
+        self.graph.add_edge("hindi_translation", END)
+        self.graph.add_edge("french_translation", END)
+
+        return self.graph
+
     def setup_graph(self, usecase):
         if usecase == "topic":
             self.build_topic_graph()
+
+        if usecase == "language":
+            self.build_language_graph()
 
         return self.graph.compile()
 
